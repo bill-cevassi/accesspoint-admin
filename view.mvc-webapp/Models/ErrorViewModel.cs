45d9312f5c4e29d43af6f809364df590a4576556
bill.ncc1631 <bill.ncc1631@outlook.com>
2021-11-29 13:23:41 -0300

    create mvc webapp

---
+++ view.mvc-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace view.mvc_webapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
