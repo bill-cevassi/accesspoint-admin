55d74622306957feb74bbe53032b735001e86fda
bill.ncc1631 <bill.ncc1631@outlook.com>
2021-11-29 13:03:52 -0300

    create mvc webapp

---
+++ accesspoint-admin.mvc.webapṕ/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace accesspoint_admin.mvc.webapṕ.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
